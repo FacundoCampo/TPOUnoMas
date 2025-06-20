1e7a3119d4e0949dc7f95b719607d1c51b19e41f
Marlene <31359415+MNB-dev@users.noreply.github.com>
2025-06-20 18:11:29 -0300

    cambios

---
+++ TPOUnoMas/src/Main.java
@@ -0,0 +1,22 @@
+import util.DataLoader;
+import views.FrmPrincipal;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+    	String titulo = "UnoMas";
+    	FrmPrincipal frame;
+
+        try {
+            DataLoader.cargarDeportes();
+            DataLoader.cargarPartidos();
+
+        	frame = new FrmPrincipal(titulo);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
